eb4828e1124815e1527576a3143d3773e65df39a
David G Chaves <davidgchaves@gmail.com>
2015-04-26 21:51:09 +0200

    Implement exercise 2.1 - Fibonacci

--- Chapter2Module.scala
+++ Chapter2Module.scala
@@ -4,5 +4,22 @@
  */
 object Chapter2Module {
 
+  /*
+   * EXERCISE 2.1: Write a recursive function to get the nth Fibonacci number
+   * The first two Fibonacci numbers are 0 and 1.
+   * The nth number is always the sum of the previous two.
+   * The sequence begins 0, 1, 1, 2, 3, 5.
+   * Your definition should use a local tail-recursive function.
+   */
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, m: Int, acc: Int): Int = n match {
+      case 0 => m
+      case _ => loop(n-1, acc, m+acc)
+    }
+
+    loop(n, 0, 1)
+  }
+
 }
 
