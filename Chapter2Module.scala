3ad33fdef96817ef56ae8093f167a760961892b3
David G Chaves <davidgchaves@gmail.com>
2015-04-26 21:52:01 +0200

    Implement exercise 2.2 - isSorted

--- Chapter2Module.scala
+++ Chapter2Module.scala
@@ -21,5 +21,26 @@ object Chapter2Module {
     loop(n, 0, 1)
   }
 
+  /*
+   * EXERCISE 2.2: Implement isSorted, which
+   * checks whether an Array[A] is sorted according to a given comparison function.
+   * NOTE1: I don't like how this exercise 'uses' ordered, very ambiguous.
+   * NOTE2: I've picked into the hint and answer and they use a much better gt (greater than) function,
+   *        which I think clarifies the exercise a bit, but it's not clear if we are talking about desc or asc
+   * NOTE3: Ok, it's descending, which is kind of awkard, because when gt(as(n),as(n+1)) is true, then we say isSorted is false... whatever
+   */
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1) true
+      else gt(as(n), as(n+1)) match {
+        case true => false
+        case _    => loop(n+1)
+      }
+    }
+
+    loop(0)
+  }
+
 }
 
