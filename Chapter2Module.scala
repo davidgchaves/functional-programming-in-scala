eb40e287e27fed95a1617bc46106613a24d04b88
David G Chaves <davidgchaves@gmail.com>
2015-04-27 18:56:54 +0200

    Implement exercise 2.3 - curry

--- Chapter2Module.scala
+++ Chapter2Module.scala
@@ -42,5 +42,12 @@ object Chapter2Module {
     loop(0)
   }
 
+  /*
+   * EXERCISE 2.3: Implement curry,
+   * which converts a function f of two arguments into a function of one argument that partially applies f.
+   */
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a,b)
+
 }
 
