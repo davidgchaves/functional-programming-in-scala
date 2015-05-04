d5f966da5655c61c8e1afbddb83c3a4285e58f24
David G Chaves <davidgchaves@gmail.com>
2015-05-04 20:36:19 +0200

    Implement exercise 3.25 - size

--- Chapter3Module.scala
+++ Chapter3Module.scala
@@ -372,5 +372,19 @@ object Chapter3Module {
   case class Leaf[A](value: A) extends Tree[A]
   case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
+
+  /*
+   * EXERCISE 3.25: Implement size, which counts the number of nodes (leaves and branches) in a tree
+   */
+  def size[A](t: Tree[A]): Int = t match {
+    case Branch(l,r) => 1 + size(l) + size(r)
+    case Leaf(_)     => 1
+  }
+  // scala> val tree1 = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+  // tree1: Chapter3Module.Branch[Int] = Branch(Branch(Leaf(1),Leaf(2)),Leaf(3))
+  //
+  // scala> size(tree1)
+  // res16: Int = 5
+
 }
 
